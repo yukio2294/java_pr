28ef067a18fe6506a7c8b12d9d933a0ce659d150
yukio2294 <yuki.o2294@gmail.com>
2018-02-12 08:19:45 +0900

    add IntTest

---
+++ IntTest.java
@@ -0,0 +1,10 @@
+public class IntTest {
+public static void main(String[] args){
+        int age = 23;
+        System.out.print("age:");
+        System.out.print(age);
+        age += 1;
+        System.out.print("\n一年後 age:");
+        System.out.print(age);
+}
+}
